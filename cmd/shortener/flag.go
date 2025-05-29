477a89fc6a9402e07545cb3586dcd1078af14b55
Gerfey <sanjia@live.ru>
2025-05-29 20:50:46 +0700

    feat: добавлена поддержка конфигурационного JSON-файла для настроек сервера

--- cmd/shortener/flag.go
+++ cmd/shortener/flag.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"os"
 )
+
+// Config структура для конфигурационного файла JSON
+type Config struct {
+	ServerAddress   string `json:"server_address"`
+	BaseURL         string `json:"base_url"`
+	FileStoragePath string `json:"file_storage_path"`
+	DatabaseDSN     string `json:"database_dsn"`
+	EnableHTTPS     bool   `json:"enable_https"`
+	CertFile        string `json:"cert_file,omitempty"`
+	KeyFile         string `json:"key_file,omitempty"`
+}
 
 // Flags содержит флаги командной строки
 type Flags struct {
@@ -14,10 +26,11 @@ type Flags struct {
 	FlagEnableHTTPS            bool
 	FlagCertFile               string
 	FlagKeyFile                string
+	FlagConfigFile             string
 }
 
 func parseFlags(args []string) Flags {
-	var flagServerRunAddress, flagServerShortenerAddress, flagDefaultFilePath, flagDefaultDatabaseDSN, flagCertFile, flagKeyFile string
+	var flagServerRunAddress, flagServerShortenerAddress, flagDefaultFilePath, flagDefaultDatabaseDSN, flagCertFile, flagKeyFile, flagConfigFile string
 	var flagEnableHTTPS bool
 
 	fs := flag.NewFlagSet("shortener", flag.ExitOnError)
@@ -29,10 +42,45 @@ func parseFlags(args []string) Flags {
 	fs.BoolVar(&flagEnableHTTPS, "s", false, "Enable HTTPS")
 	fs.StringVar(&flagCertFile, "cert", "server.crt", "Path to TLS certificate file")
 	fs.StringVar(&flagKeyFile, "key", "server.key", "Path to TLS key file")
+	fs.StringVar(&flagConfigFile, "c", "", "Path to configuration file")
+	fs.StringVar(&flagConfigFile, "config", "", "Path to configuration file (shorthand for -c)")
 
-	err := fs.Parse(args)
-	if err != nil {
+	if err := fs.Parse(args); err != nil {
 		return Flags{}
+	}
+
+	if envConfigFile := os.Getenv("CONFIG"); envConfigFile != "" {
+		flagConfigFile = envConfigFile
+	}
+
+	if flagConfigFile != "" {
+		configData, err := os.ReadFile(flagConfigFile)
+		if err == nil {
+			var config Config
+			if err := json.Unmarshal(configData, &config); err == nil {
+				if flagServerRunAddress == ":8080" && config.ServerAddress != "" {
+					flagServerRunAddress = config.ServerAddress
+				}
+				if flagServerShortenerAddress == "http://localhost:8080" && config.BaseURL != "" {
+					flagServerShortenerAddress = config.BaseURL
+				}
+				if flagDefaultFilePath == "" && config.FileStoragePath != "" {
+					flagDefaultFilePath = config.FileStoragePath
+				}
+				if flagDefaultDatabaseDSN == "" && config.DatabaseDSN != "" {
+					flagDefaultDatabaseDSN = config.DatabaseDSN
+				}
+				if !flagEnableHTTPS && config.EnableHTTPS {
+					flagEnableHTTPS = config.EnableHTTPS
+				}
+				if flagCertFile == "server.crt" && config.CertFile != "" {
+					flagCertFile = config.CertFile
+				}
+				if flagKeyFile == "server.key" && config.KeyFile != "" {
+					flagKeyFile = config.KeyFile
+				}
+			}
+		}
 	}
 
 	if flagEnableHTTPS {
@@ -69,5 +117,5 @@ func parseFlags(args []string) Flags {
 		flagKeyFile = envKeyFile
 	}
 
-	return Flags{flagServerRunAddress, flagServerShortenerAddress, flagDefaultFilePath, flagDefaultDatabaseDSN, flagEnableHTTPS, flagCertFile, flagKeyFile}
+	return Flags{flagServerRunAddress, flagServerShortenerAddress, flagDefaultFilePath, flagDefaultDatabaseDSN, flagEnableHTTPS, flagCertFile, flagKeyFile, flagConfigFile}
 }
